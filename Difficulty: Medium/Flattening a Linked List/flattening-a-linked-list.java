b9a4fd9e4466ccbcbd0a5c20ed0c0e5e98ac54e6
Aparna Kanda <119393668+aparnakanda@users.noreply.github.com>
2026-01-03 21:15:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.java
@@ -0,0 +1,42 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node bottom;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        bottom = null;
+    }
+}
+*/
+class Solution {
+    private Node merge(Node a, Node b) {
+        if (a == null) return b;
+        if (b == null) return a;
+
+        Node result;
+
+        if (a.data < b.data) {
+            result = a;
+            result.bottom = merge(a.bottom, b);
+        } else {
+            result = b;
+            result.bottom = merge(a, b.bottom);
+        }
+
+        return result;
+    }
+    public Node flatten(Node root) {
+        // code here
+        if (root == null || root.next == null)
+        {
+                        return root;
+        }
+        root.next = flatten(root.next);
+        root = merge(root, root.next);
+        return root;
+        
+    }
+}
