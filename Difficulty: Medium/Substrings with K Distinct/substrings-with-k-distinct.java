0ad581f7bc929c1c8a9d06ca1d7623a2cc87d930
Aparna Kanda <119393668+aparnakanda@users.noreply.github.com>
2026-01-10 21:16:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countSubstr(String s, int k) {
+        //  code here
+         if (k == 0) return 0;
+        return (int)(atMostK(s, k) - atMostK(s, k - 1));
+    }
+
+    private long atMostK(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0, distinct = 0;
+        long count = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int r = s.charAt(right) - 'a';
+            if (freq[r] == 0) distinct++;
+            freq[r]++;
+
+            while (distinct > k) {
+                int l = s.charAt(left) - 'a';
+                freq[l]--;
+                if (freq[l] == 0) distinct--;
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+        return count;
+    }
+}
